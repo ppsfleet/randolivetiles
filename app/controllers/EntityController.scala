9f2621cec82303a8d2331dc6d770734160a6269c
Tjiho <tom.darboux@laposte.net>
2019-03-26 17:55:46 +0100

    add Q3

--- app/controllers/EntityController.scala
+++ app/controllers/EntityController.scala
@@ -1,28 +1,51 @@
 package controllers
 
+
 import javax.inject._
+import scala.concurrent._
+import scala.concurrent.duration._
 import play.api._
 import play.api.Logger
 import play.api.data.Form
-import play.api.libs.json.Json
 import play.api.mvc._
-
+import play.api.Play.current
+import play.api.libs.ws._
+import play.api.http.HttpEntity
+import play.api.libs.json._
 
 @Singleton
-class EntityController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class EntityController @Inject()(cc: ControllerComponents,ws: WSClient) (implicit ec:ExecutionContext) extends AbstractController(cc) {
 
 
-  def locate: Action[AnyContent] = Action { implicit request =>
+  def locate: Action[AnyContent] = Action.async { implicit request =>
     request.body.asJson.map { json =>
         (json \ "places").asOpt[List[String]].map { places =>
-        val entities = places.map(value => "geoloc of "+value)
-        val result = Map("geolocs" -> entities)
-        Ok(Json.toJson(result))
+          Future.sequence( //on va convertir notre "liste de futur" en "futur de liste"
+            places.map(value => getLocation(value)) //on recupere les coordonnes pour chaque places
+          ).map(
+            res => Ok(Json.toJson(res.flatten)) //on flatten et on renvoie en json
+          )
         }.getOrElse {
-        BadRequest("Missing parameter places")
+          Future(BadRequest("Missing parameter places"))
         }
     }.getOrElse {
-        BadRequest("Expecting Json data")
+        Future(BadRequest("Expecting Json data"))
     }    
+  }
+
+  //renvoi les locations d'une place sous forme de futur[seq[location]]
+  def getLocation(place:String) : Future[Seq[String]] = {
+    val query: String = "http://fr.dbpedia.org/sparql?default-graph-uri=&query=select+distinct+%3Fcoord+where+%7B+dbpedia-fr%3A"+place+"+georss%3Apoint+%3Fcoord+%7D+LIMIT+100&format=application%2Fsparql-results%2Bjson"
+    val promiseOfString: Future[WSResponse] = ws.url(query).get()
+    promiseOfString.map(res => {
+        val json: JsValue = Json.parse(res.body)
+        (json \ "results" \\ "value").map( value => {
+          value match {
+            case JsString(str) => str
+            case JsNull => ""
+          }
+        })
+    }
+    ) 
   }
 }
