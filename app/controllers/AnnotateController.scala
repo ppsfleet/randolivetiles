46c2ad7d5fae4ca8ec70459063cd963d55f7625c
Elio Maisonneuve <elio.maisonneuve@eisti.fr>
2019-03-26 18:10:19 +0100

    Provide text annotations from dbpedia

--- app/controllers/AnnotateController.scala
+++ app/controllers/AnnotateController.scala
@@ -6,20 +6,32 @@ import play.api.Logger
 import play.api.data.Form
 import play.api.libs.json.Json
 import play.api.mvc._
+import play.api.Play.current
+import play.api.libs.ws._
+import play.api.http.HttpEntity
+import scala.concurrent._
+import scala.concurrent.duration._
 
 
 @Singleton
-class AnnotateController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  def annotate: Action[AnyContent] = Action { implicit request =>
+class AnnotateController @Inject()(cc: ControllerComponents,ws: WSClient) (implicit ec:ExecutionContext) extends AbstractController(cc) {
+  def annotate: Action[AnyContent] = Action.async { implicit request =>
     request.body.asJson.map { json =>
-        (json \ "text").asOpt[String].map { text =>
-        val result = Map("places" -> List("uri1", "uri2", text))
-        Ok(Json.toJson(result))
+        (json \ "text").asOpt[String].map { text2annotate =>
+            requestApi(text2annotate).map(res =>
+                Ok(Json.toJson( Json.parse(res.body) \\ "@URI" ))
+            )
         }.getOrElse {
-        BadRequest("Missing parameter text")
+            Future(BadRequest("Missing parameter text"))
         }
     }.getOrElse {
-        BadRequest("Expecting Json data")
-    }    
+        Future(BadRequest("Expecting Json data"))
+    }
   }
+
+   def requestApi(text:String) = {
+       ws.url("http://icc.pau.eisti.fr/rest/annotate")
+           .addHttpHeaders("Accept" -> "application/json")
+           .withQueryStringParameters("text" -> text).get()
+   }
 }
