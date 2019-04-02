343d347ab95b4e1b43d5b06f4ec3c642ea1158fb
Elio Maisonneuve <elio.maisonneuve@eisti.fr>
2019-04-02 14:49:57 +0200

    non blocking tile generation

--- satiles/satiles.py
+++ satiles/satiles.py
@@ -1,10 +1,13 @@
 from aiohttp import web
 from aiojobs.aiohttp import setup, spawn
+from concurrent.futures import ProcessPoolExecutor
+from zipfile import ZipFile
+from gdal2tiles import generate_tiles
+import asyncio
 import aiohttp
 import asyncio
-from zipfile import ZipFile
 import os
-from gdal2tiles import generate_tiles
+import re
 
 
 async def downloadfile(url, filename):
@@ -19,20 +22,35 @@ async def downloadfile(url, filename):
                         break
                     zipfile.write(chunk)
 
-            extract_img(filename)
-            await gen_tiles(filename)
 
-
-async def gen_tiles(filename):
-            generate_tiles(filename + ".jp2", filename, np_processes=8, zoom="6-11")
-
-
-def extract_img(filename):
+async def extract_img(filename):
     with ZipFile(filename + ".zip", 'r') as zfile:
         for member in zfile.infolist():
-            if "TCI_10m" in member.filename:
-                member.filename = filename + ".jp2"
+            if re.match(r'(.*)(TCI)(.)(?!(20m|30m))(.*)', member.filename):
+                member.filename = os.path.basename(member.filename)
                 zfile.extract(member)
+                yield member.filename
+
+
+def gen_tiles_wrapper(sat_img):
+    generate_tiles(sat_img, sat_img.split(".")[0], np_processes=8, zoom="6-11")
+
+
+async def run_tile_generation(file_extractor):
+    async for sat_img in file_extractor:
+        print("starting generation on {}".format(sat_img))
+        with ProcessPoolExecutor() as process_pool:
+            loop = asyncio.get_event_loop()
+            await loop.run_in_executor(
+                process_pool,
+                gen_tiles_wrapper,
+                sat_img,
+            )
+
+
+async def process_url(url, filename):
+    await downloadfile(url, filename)
+    await run_tile_generation(extract_img(filename))
 
 
 async def sat_img_downloader(request):
@@ -41,9 +59,9 @@ async def sat_img_downloader(request):
     filename = img_url.split("/")[-2]
     print(img_url)
 
-    await spawn(request, downloadfile(img_url, filename))
+    await spawn(request, process_url(img_url, filename))
 
-    response = web.Response(text="bite", status=202)
+    response = web.Response(text=os.path.join(os.getcwd(), filename), status=202)
     await response.prepare(request)
     await response.write_eof()
 
